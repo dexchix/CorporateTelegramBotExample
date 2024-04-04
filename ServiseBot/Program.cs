41a96bb88d9f82552c2c91559bb7622c13714413
Denis <Denis@DESKTOP-M7N3HVN>
2024-04-05 00:51:29 +0300

    Add project files.

---
+++ ServiseBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ServiseBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var client = new TelegramBotClient("6858392505:AAHXlxagKKKFiZE0N5XUGbRwTnYxJa6Az-A");
+            client.StartReceiving(Update, Error);
+
+
+            Console.ReadLine();
+        }
+
+        async private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+           
+        }
+
+        async private static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var dawd = client.
+            var message = update.Message; 
+            if(message.Text != null)
+            {
+               await client.SendTextMessageAsync(message.Chat.Id, "Привет");
+            }
+        }
+    }
+}
